eb5ccbee2b779de9e5622996c7952877250f5483
felipe-feEee <fmcicq@gmail.com>
2025-12-03 14:30:04 -0300

    Update main.js

--- MonaNote/js/main.js
+++ MonaNote/js/main.js
@@ -988,7 +988,6 @@ function loadArticle(categoria, id) {
 
   // após renderizar:
   setActiveArticle(id);
-  window.scrollTo({ top: 0, behavior: 'smooth' });
 
   container.innerHTML = `
     <h1 id="article-title">${artigo.titulo}</h1>
@@ -1015,6 +1014,7 @@ function loadArticle(categoria, id) {
     });
   }
   closeSidebarIfMobile();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
 
 /* ============================
