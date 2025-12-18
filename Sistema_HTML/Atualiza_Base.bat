4f623514c04e0e1028c4041f7cb02cb96b418e42
felipe-feEee <fmcicq@gmail.com>
2025-12-18 19:25:29 -0300

    Delete Sistema_HTML directory

--- Sistema_HTML/Atualiza_Base.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Executando Cria JSON.ps1...
-powershell -NoProfile -ExecutionPolicy Bypass -File "\\snbrcampmix005.la.corp.cargill.com\vs02_INT_NEOGRID_PD\Sistema_HTML\script\Cria JSON.ps1"
-
-exit
